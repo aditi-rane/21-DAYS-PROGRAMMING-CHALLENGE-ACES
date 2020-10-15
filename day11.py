c3492c9dd0473718fd6945b98787b3856c0b614d
aditi-rane <66665685+aditi-rane@users.noreply.github.com>
2020-10-15 21:47:18 +0530

    Explored more about matplotlib

---
+++ day11.py
@@ -0,0 +1,94 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Line plot
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+ax.grid()
+
+fig.savefig("test.png")
+plt.show()
+
+#------------------------------------------------------
+
+#Multiple plots
+x1 = np.linspace(0.0, 5.0)
+x2 = np.linspace(0.0, 2.0)
+
+y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
+y2 = np.cos(2 * np.pi * x2)
+
+plt.subplot(2, 1, 1)
+plt.plot(x1, y1, 'o-')
+plt.title('A tale of 2 subplots')
+plt.ylabel('Damped oscillation')
+
+plt.subplot(2, 1, 2)
+plt.plot(x2, y2, '.-')
+plt.xlabel('time (s)')
+plt.ylabel('Undamped')
+
+plt.show()
+
+#------------------------------------------------------------
+
+#3D plotting
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+ax.set_zlim(-1.01, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+fig.colorbar(surf, shrink=0.5, aspect=5)
+plt.show()
+
+#------------------------------------------------------------
+#Polar plot
+r = np.arange(0, 2, 0.01)
+theta = 2 * np.pi * r
+
+ax = plt.subplot(111, projection='polar')
+ax.plot(theta, r)
+ax.set_rmax(2)
+ax.set_rticks([0.5, 1, 1.5, 2])
+ax.set_rlabel_position(-22.5)
+ax.grid(True)
+
+ax.set_title("A line plot on a polar axis", va='bottom')
+plt.show()
+
+#-----------------------------------------------------------
+#Legends
+a = b = np.arange(0, 3, .02)
+c = np.exp(a)
+d = c[::-1]
+
+fig, ax = plt.subplots()
+ax.plot(a, c, 'k--', label='Model length')
+ax.plot(a, d, 'k:', label='Data length')
+ax.plot(a, c + d, 'k', label='Total message length')
+
+legend = ax.legend(loc='upper center', shadow=True, fontsize='x-large')
+legend.get_frame().set_facecolor('C0')
+plt.show()
